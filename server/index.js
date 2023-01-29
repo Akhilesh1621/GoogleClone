8f74170ad6d5e4b5ead8b56ce9b5b2b04142bbf9
Akhilesh1621 <akhileshdevop@gmail.com>
2023-01-29 21:57:34 +0530

    callingsignupAPI

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
+const cors = require ("cors");
 
 const PORT = process.env.PORT | 3001;
 
@@ -8,8 +9,15 @@ const PORT = process.env.PORT | 3001;
 const app = express();
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(authRouter);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const DB ="mongodb+srv://Akhilesh:akhiakhi1621@cluster0.detn6vd.mongodb.net/?retryWrites=true&w=majority";
 
